4a37017010897629f22bc0633dda60504cbabe3c
Slava <slava@artsurf.pro>
2022-04-18 14:20:35 +0300

    add ci

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'bp_landing_art_app',
+            script: 'npm',
+            args: 'run start',
+            instances: 1,
+            autorestart: true,
+        },
+    ],
+};
