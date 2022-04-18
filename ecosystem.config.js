eca0a5d36d0f4cd90d14fa22c471781271da74dc
Slava <slava@artsurf.pro>
2022-04-18 15:54:25 +0300

    update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'bp_landing_art_app',
             script: 'npm',
-            args: 'run start',
+            args: "start -- --port 3014",
             instances: 1,
             autorestart: true,
         },
