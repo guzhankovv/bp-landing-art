4c9ceb4c14b45e96d33d6c455098de696793b0a3
anna <anna.xanova.02@gmail.com>
2022-05-15 10:36:57 +0500

    nikvsio

--- static/timeLineArr.js
+++ static/timeLineArr.js
@@ -173,65 +173,65 @@ export default {
           link: 'https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/34421031415970950286101163622191489157320889034924944611203052246431860523009'
         }
       ]
+    },
+    {
+      artist: 'NIKVSIO',
+      date: '15.05.2022',
+      avatar: '/images/avatars/nikvsio.png',
+      nftList: [
+        {
+          name: 'KNIGHT OF LIGHT',
+          prcie: '0.15 ETH 1/1',
+          img: 'knight.jpg',
+          link: 'https://www.dropbox.com/s/bbvt2e5mi9pbvp2/NIKVSIO.png?dl=0'
+        },
+        {
+          name: 'BOLD POINT',
+          prcie: '0,09 ETH 1/3',
+          img: 'bold.jpg',
+          link: 'https://www.dropbox.com/s/hngzhqclhc7f4ur/NIKVSIO2.png?dl=0'
+        },
+        {
+          name: 'GOLDEN CROW',
+          prcie: '0.07 ETH 1/5',
+          img: 'crow.jpg',
+          link: 'https://www.dropbox.com/s/7avc8fnhry3khzh/NIKVSIO3.png?dl=0'
+        }
+      ]
+    },
+    {
+      artist: 'Tim Yarzhombek',
+      date: '18.05.2022',
+      avatar: '/images/avatars/timYar.png',
+      nftList: [
+        {
+          name: 'wizard/viza',
+          prcie: '0.15 ETH 1/1',
+          img: 'visard.jpg',
+          link: 'https://www.dropbox.com/s/pxmpdax8clex12e/crown%28tim%20yarzhombek%29.jpg?dl=0'
+        },
+        {
+          name: 'tyrany/teraphy',
+          prcie: '0,09 ETH 1/3',
+          img: 'teraphy.jpg',
+          link: 'https://www.dropbox.com/s/t4saj2wj63tx0cp/gloves%28tim%20yarzhombek%29.jpg?dl=0'
+        },
+        {
+          name: 'gLOVES',
+          prcie: '0.07 ETH 1/5',
+          img: 'gloves.jpg',
+          link: 'https://www.dropbox.com/s/0slba89no133pty/teraphy%28tim%20yarzhombek%29.jpg?dl=0'
+        },
+        {
+          name: 'crowN',
+          prcie: '0.05 ETH 1/7',
+          img: 'crown.jpg',
+          link: 'https://www.dropbox.com/s/8xvhleuv224scey/visard%28tim%20yarzhombek%29.jpg?dl=0'
+        }
+      ]
     }
     // {
-    //   artist: 'NIKVSIO',
-    //   date: '15.05.2022',
-    //   avatar: '/images/avatars/nikvsio.png',
-    //   nftList: [
-    //     {
-    //       name: 'KNIGHT OF LIGHT',
-    //       prcie: '0.15 ETH 1/1',
-    //       img: 'knight.jpg',
-    //       link: 'https://www.dropbox.com/s/bbvt2e5mi9pbvp2/NIKVSIO.png?dl=0'
-    //     },
-    //     {
-    //       name: 'BOLD POINT',
-    //       prcie: '0,09 ETH 1/3',
-    //       img: 'bold.jpg',
-    //       link: 'https://www.dropbox.com/s/hngzhqclhc7f4ur/NIKVSIO2.png?dl=0'
-    //     },
-    //     {
-    //       name: 'GOLDEN CROW',
-    //       prcie: '0.07 ETH 1/5',
-    //       img: 'crow.jpg',
-    //       link: 'https://www.dropbox.com/s/7avc8fnhry3khzh/NIKVSIO3.png?dl=0'
-    //     }
-    //   ]
-    // },
-    // {
-    //   artist: 'Tim Yarzhombek',
-    //   date: '18.05.2022',
-    //   avatar: '/images/avatars/timYar.png',
-    //   nftList: [
-    //     {
-    //       name: 'wizard/viza',
-    //       prcie: '0.15 ETH 1/1',
-    //       img: 'visard.jpg',
-    //       link: 'https://www.dropbox.com/s/pxmpdax8clex12e/crown%28tim%20yarzhombek%29.jpg?dl=0'
-    //     },
-    //     {
-    //       name: 'tyrany/teraphy',
-    //       prcie: '0,09 ETH 1/3',
-    //       img: 'teraphy.jpg',
-    //       link: 'https://www.dropbox.com/s/t4saj2wj63tx0cp/gloves%28tim%20yarzhombek%29.jpg?dl=0'
-    //     },
-    //     {
-    //       name: 'gLOVES',
-    //       prcie: '0.07 ETH 1/5',
-    //       img: 'gloves.jpg',
-    //       link: 'https://www.dropbox.com/s/0slba89no133pty/teraphy%28tim%20yarzhombek%29.jpg?dl=0'
-    //     },
-    //     {
-    //       name: 'crowN',
-    //       prcie: '0.05 ETH 1/7',
-    //       img: 'crown.jpg',
-    //       link: 'https://www.dropbox.com/s/8xvhleuv224scey/visard%28tim%20yarzhombek%29.jpg?dl=0'
-    //     }
-    //   ]
-    // },
-    // {
-    //   artist: 'Vladimir Ayuev',
+    //   artist: 'vladimir ayuev',
     //   date: '21.05.2022',
     //   avatar: '/images/avatars/vladimir.png',
     //   nftList: [
@@ -254,6 +254,56 @@ export default {
     //       link: 'https://www.dropbox.com/s/08z1juuras971xl/ayuev_throne1_out.mp4?dl=0'
     //     }
     //   ]
-    // }
+    // },
+    // {
+    //   artist: 'thisworldout',
+    //   date: '24.05.2022',
+    //   avatar: '/images/avatars/thisWorldOut.png',
+    //   nftList: [
+    //     {
+    //       name: 'Human Mage',
+    //       prcie: '0.15 ETH 1/1',
+    //       img: 'humanMage.jpg',
+    //       link: '/'
+    //     },
+    //     {
+    //       name: 'Magic Orb',
+    //       prcie: '0,09 ETH 1/3',
+    //       img: 'magicOrb.jpg',
+    //       link: '/'
+    //     },
+    //     {
+    //       name: 'Arcanite Sword',
+    //       prcie: '0.07 ETH 1/5',
+    //       img: 'arcaniteSword.jpg',
+    //       link: '/'
+    //     },
+    //     {
+    //       name: 'CryptoPotion of Mana',
+    //       prcie: '0.03 ETH 1/9',
+    //       img: 'cryptoPotionOfMana.jpg',
+    //       link: '/'
+    //     }
+    //   ]
+    // },
+    // {
+    //   artist: 'bxb.std',
+    //   date: '27.05.2022',
+    //   avatar: '/images/avatars/bxb.png',
+    //   nftList: [
+    //     {
+    //       name: 'Lagertha',
+    //       prcie: '0.15 ETH 1/1',
+    //       img: 'lagertha.jpg',
+    //       link: '/'
+    //     },
+    //     {
+    //       name: 'Anariel',
+    //       prcie: '0,09 ETH 1/3',
+    //       img: 'anariel.jpg',
+    //       link: '/'
+    //     },
+    //   ]
+    // },
   ]
 }
